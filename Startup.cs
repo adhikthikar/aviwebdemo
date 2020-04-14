f5fc6e746538025346a7f36f463a32a4f5cf297c
aadhikthikar <aadhikthikar@dxc.com>
2020-04-14 10:03:32 +1000

    update message

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace AviWebDemo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Azure!");
             });
         }
     }
